abde7ac4f48858e94c07f14fe251e51c27345ee2
Alaap Joshi <alaapjoshi@gmail.com>
2022-10-10 18:22:43 -0400

    kjsxhdx:wq

--- Windows/shutdown.py
+++ Windows/shutdown.py
@@ -10,29 +10,14 @@ def sound():
 
 
 def countdown():
-    print("Locking in 10...")
-    time.sleep(2)
-    print("9...")
-    time.sleep(2)
-    print("8...")
-    time.sleep(2)
-    print("7...")
-    time.sleep(2)
-    print("6...")
-    time.sleep(2)
-    print("5...")
-    time.sleep(2)
-    print("4...")
-    # dramatic effect
-    time.sleep(3)
-    print("3..")
-    time.sleep(3)
-    print("2..")
-    time.sleep(3)
-    print("1..")
-    time.sleep(0.5)
-    print("Goodbye")
-    time.sleep(1.5)
+    i = 10
+
+    while i > 0:
+        j = str(i)
+        print("Shutting Down in " + j + "...")
+        time.sleep(2.2)
+        i = i - 1
+    print("Goodbye :)")
     ctypes.windll.user32.LockWorkStation()
 
 
